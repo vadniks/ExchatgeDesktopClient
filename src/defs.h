db4fd12dc57e342dfe492b6f4682950fdf915e65
vadniks <whatever@fake.some>
2023-06-01 01:59:28 +0300

    Structure, logic & ui

--- src/defs.h
+++ src/defs.h
@@ -5,8 +5,6 @@
     typedef struct { x } this_t; \
     static this_t* this = NULL;
 
-#define _MESSAGE_BODY_SIZE (1 << 10)
-
 #ifdef __clang__
 #   define nullable _Nullable
 #   define staticAssert(x) _Static_assert(x, "")
@@ -21,6 +19,8 @@ typedef unsigned char byte;
 extern const char* APP_TITLE;
 extern const char* NET_HOST;
 
+#define NET_MESSAGE_BODY_SIZE (1 << 10)
+
 extern const int APP_WIDTH;
 extern const int APP_HEIGHT;
 extern const int UI_UPDATE_PERIOD;
@@ -28,11 +28,10 @@ extern const int NET_UPDATE_PERIOD;
 extern const int NET_PORT;
 extern const int NET_RECEIVE_BUFFER_SIZE;
 extern const int NET_MESSAGE_HEAD_SIZE;
-extern const int NET_MESSAGE_BODY_SIZE;
 
 typedef struct {
     int flags;
     long timestamp;
     int size;
-    byte body[_MESSAGE_BODY_SIZE]; // here's why constants are usually declared via macros
+    byte body[NET_MESSAGE_BODY_SIZE]; // here's why constants are usually declared via macros
 } message;
