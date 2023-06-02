66e956c7c22132c7a04d1686e51bdf0bf63ad7d7
vadniks <whatever@fake.some>
2023-06-02 22:25:14 +0300

    Structure & logic

--- src/main.c
+++ src/main.c
@@ -6,8 +6,9 @@
 int main() {
     sodium_init();
 
-    unsigned char buf[1024] = {0}; // TODO: test only
-    unsigned long unpaddedLen = 1008;
+    // whole message size = head + body = 24 + 1024 = 1048 so we need a 1088 sized receive buffer (1048 + mac + nonce = 1048 + 16 + 24)
+    unsigned char buf[1088] = {0}; // TODO: test only
+    unsigned long unpaddedLen = 1072;
     for (unsigned i = 0; i < 256; i++) buf[i] = i;
     unsigned long paddedLen = 0;
     unsigned long blockLen = 16;
@@ -15,7 +16,7 @@ int main() {
     for (unsigned i = 0; i < 256; i++) printf("%u ", buf[i]);
     printf("\n");
 
-    int res = sodium_pad(&paddedLen, buf, unpaddedLen, blockLen, 1024);
+    int res = sodium_pad(&paddedLen, buf, unpaddedLen, blockLen, 1088);
     printf("%d %lu\n", res, paddedLen);
     for (unsigned i = 0; i < paddedLen; i++) printf("%u_", buf[i]);
     printf(" #\n");
@@ -25,7 +26,7 @@ int main() {
     for (unsigned i = 0; i < 256; i++) printf("%u ", buf[i]);
     printf("\n");
 
-    // TODO: maximum message length is 1008 to fit it in the 1024 long buffer with padding applied
+    // TODO: maximum message length is 1072 to fit it in the 1088 long buffer with padding applied
 
 //    if (!lcInit()) return 1;
 //    lcLoop();
