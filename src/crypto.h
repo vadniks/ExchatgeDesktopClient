739a758f7b4744f5ebf2a739763acc60df42b450
vadniks <whatever@fake.some>
2023-06-04 00:12:12 +0300

    Structure & logic

--- src/crypto.h
+++ src/crypto.h
@@ -9,10 +9,10 @@ typedef struct {
     unsigned unpaddedSize;
 } CryptoCryptDetails;
 
-byte* nullable cryptoInit(byte* serverPublicKey, CryptoCryptDetails* cryptDetails);
+byte* nullable cryptoInit(byte* serverPublicKey, CryptoCryptDetails* cryptDetails); // returns client public key
 unsigned cryptoPublicKeySize();
-unsigned cryptoEncryptedSize();
-unsigned cryptoPaddedSize();
+unsigned cryptoEncryptedSize(); // returns size of mac (16) + encrypted bytes (cryptoDetails.unpaddedSize) + nonce (24)
+unsigned cryptoPaddedSize(); // returns size of unencrypted padded size
 byte* nullable cryptoEncrypt(byte* bytes); // returns mac (tag) + encrypted bytes + nonce
 byte* nullable cryptoDecrypt(byte* bytes); // consumes what is returned by encrypt
 void cryptoClean();
