6f201e9f79a1ee374fffa0d165c207d644f36469
vadniks <whatever@fake.some>
2023-06-12 12:59:03 +0300

    Structure & logic

--- src/crypto.h
+++ src/crypto.h
@@ -11,8 +11,8 @@ typedef struct {
 
 byte* nullable cryptoInit(byte* serverPublicKey, CryptoCryptDetails* cryptDetails); // returns client public key
 unsigned cryptoPublicKeySize();
-unsigned cryptoEncryptedSize(); // returns size of mac (16) + encrypted bytes (cryptoDetails.unpaddedSize) + nonce (24)
-unsigned cryptoPaddedSize(); // returns size of unencrypted padded size
+unsigned cryptoEncryptedSize(); // returns size of mac (16) + encrypted bytes (cryptoDetails.unpaddedSize) + nonce (24) = 1112
+unsigned cryptoPaddedSize(); // returns size of unencrypted padded size (1072)
 byte* nullable cryptoEncrypt(byte* bytes); // returns mac (tag) + encrypted bytes + nonce
 byte* nullable cryptoDecrypt(byte* bytes); // consumes what is returned by encrypt
 void cryptoClean();
--- src/net.c
+++ src/net.c
@@ -23,12 +23,14 @@ THIS( // TODO: check client's authentication by token
 #pragma clang diagnostic pop
 
 static const int PORT = 8080;
-static const unsigned MESSAGE_HEAD_SIZE = sizeof(int) * 4 + sizeof(long); // 24
+static const unsigned MESSAGE_HEAD_SIZE = sizeof(int) * 6 + sizeof(long); // 32
 static const unsigned MESSAGE_BODY_SIZE = 1 << 10; // 1024
-static const unsigned MESSAGE_SIZE = MESSAGE_HEAD_SIZE + MESSAGE_BODY_SIZE; // 1048
+static const unsigned MESSAGE_SIZE = MESSAGE_HEAD_SIZE + MESSAGE_BODY_SIZE; // 1056
 static const unsigned PADDING_BLOCK_SIZE = 16;
 static const int FLAG_UNAUTHENTICATED = 0x7ffffffe;
 static const int FLAG_FINISH = 0x7fffffff;
+static const unsigned INT_SIZE = sizeof(int);
+static const unsigned LONG_SIZE = sizeof(long);
 
 typedef struct {
     // begin head
@@ -37,6 +39,8 @@ typedef struct {
     unsigned size; // actual size of th payload
     unsigned index; // message part index if the whole message cannot fit in boy
     unsigned count; // total count of message parts
+    unsigned from; // id of the sender
+    unsigned to; // id of the receiver
     // end head
 
 #pragma clang diagnostic push
@@ -74,6 +78,8 @@ static void initiateSecuredConnection() {
     msg->size = MESSAGE_BODY_SIZE;
     msg->index = 0;
     msg->count = 1;
+    msg->from = 255;
+    msg->to = 255;
 
     const char test[] = "Test connection"; // TODO: test only
     const unsigned testLen = sizeof test / sizeof *test;
@@ -127,13 +133,14 @@ static bool isDataAvailable() {
 
 static Message* unpackMessage(byte* buffer) {
     Message* msg = SDL_malloc(sizeof *msg);
-    const unsigned intSize = sizeof(int), longSize = sizeof(long);
 
-    SDL_memcpy(&(msg->flag), buffer, intSize);
-    SDL_memcpy(&(msg->timestamp), buffer + intSize, longSize);
-    SDL_memcpy(&(msg->size), buffer + intSize + longSize, intSize);
-    SDL_memcpy(&(msg->index), buffer + intSize * 2 + longSize, intSize);
-    SDL_memcpy(&(msg->count), buffer + intSize * 3 + longSize, intSize);
+    SDL_memcpy(&(msg->flag), buffer, INT_SIZE);
+    SDL_memcpy(&(msg->timestamp), buffer + INT_SIZE, LONG_SIZE);
+    SDL_memcpy(&(msg->size), buffer + INT_SIZE + LONG_SIZE, INT_SIZE);
+    SDL_memcpy(&(msg->index), buffer + INT_SIZE * 2 + LONG_SIZE, INT_SIZE);
+    SDL_memcpy(&(msg->count), buffer + INT_SIZE * 3 + LONG_SIZE, INT_SIZE);
+    SDL_memcpy(&(msg->from), buffer + INT_SIZE * 4 + LONG_SIZE, INT_SIZE);
+    SDL_memcpy(&(msg->to), buffer + INT_SIZE * 5 + LONG_SIZE, INT_SIZE);
     SDL_memcpy(&(msg->body), buffer + MESSAGE_HEAD_SIZE, MESSAGE_BODY_SIZE);
 
     SDL_free(buffer);
@@ -142,13 +149,14 @@ static Message* unpackMessage(byte* buffer) {
 
 static byte* packMessage(Message* msg) {
     byte* buffer = SDL_calloc(MESSAGE_SIZE, sizeof(char));
-    const unsigned intSize = sizeof(int), longSize = sizeof(long);
 
-    SDL_memcpy(buffer, &(msg->flag), intSize);
-    SDL_memcpy(buffer + intSize, &(msg->timestamp), longSize);
-    SDL_memcpy(buffer + intSize + longSize, &(msg->size), intSize);
-    SDL_memcpy(buffer + intSize * 2 + longSize, &(msg->index), intSize);
-    SDL_memcpy(buffer + intSize * 3 + longSize, &(msg->count), intSize);
+    SDL_memcpy(buffer, &(msg->flag), INT_SIZE);
+    SDL_memcpy(buffer + INT_SIZE, &(msg->timestamp), LONG_SIZE);
+    SDL_memcpy(buffer + INT_SIZE + LONG_SIZE, &(msg->size), INT_SIZE);
+    SDL_memcpy(buffer + INT_SIZE * 2 + LONG_SIZE, &(msg->index), INT_SIZE);
+    SDL_memcpy(buffer + INT_SIZE * 3 + LONG_SIZE, &(msg->count), INT_SIZE);
+    SDL_memcpy(buffer + INT_SIZE * 4 + LONG_SIZE, &(msg->from), INT_SIZE);
+    SDL_memcpy(buffer + INT_SIZE * 5 + LONG_SIZE, &(msg->to), INT_SIZE);
     SDL_memcpy(buffer + MESSAGE_HEAD_SIZE, &(msg->body), MESSAGE_BODY_SIZE);
 
     SDL_free(msg);
@@ -169,6 +177,7 @@ void netListen() {
 
     switch (this->state) {
         case STATE_READY:
+            if (msg) SDL_Log("%u %lu %u %u %u %u %u\n", msg->flag, msg->timestamp, msg->size, msg->index, msg->count, msg->from, msg->to); // TODO: test only
             if (msg) this->onMessageReceived(msg->body);
             break;
     }
@@ -177,12 +186,16 @@ void netListen() {
 }
 
 void netSend(byte* message, unsigned size) {
+    assert(size <= MESSAGE_BODY_SIZE);
+
     Message* msg = SDL_malloc(sizeof *msg);
     msg->flag = 0;
     msg->timestamp = 0;
     msg->size = size;
     msg->index = 0;
     msg->count = 0;
+    msg->from = 255;
+    msg->to = 255;
 
     SDL_memset(msg->body, 0, MESSAGE_BODY_SIZE);
     SDL_memcpy(&(msg->body), message, size);
@@ -192,7 +205,7 @@ void netSend(byte* message, unsigned size) {
     byte* encryptedBuffer = cryptoEncrypt(buffer);
     if (!encryptedBuffer) return;
 
-    SDLNet_TCP_Send(this->socket, buffer, (int) this->receiveBufferSize);
+    SDLNet_TCP_Send(this->socket, encryptedBuffer, (int) this->receiveBufferSize);
     SDL_free(encryptedBuffer);
 }
 
