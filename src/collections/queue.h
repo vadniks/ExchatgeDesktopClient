c2a4e99d39d9609a643adbedc7e5e4661196d5ea
vadniks <whatever@fake.some>
2023-06-22 14:45:35 +0300

    Structure, login UI & UX

--- src/collections/queue.h
+++ src/collections/queue.h
@@ -10,6 +10,6 @@ typedef void (*QueueDeallocator)(void*);
 
 Queue* queueInit(QueueDeallocator nullable deallocator);
 void queuePush(Queue* queue, void* nullable value);
-void* nullable queuePop(Queue* queue);
+void* nullable queuePop(Queue* queue); // returns stored value that must be deallocated by a caller as reference to the value gets deleted
 unsigned queueSize(const Queue* queue);
 void queueDestroy(Queue* queue); // all values that are still remain inside a queue at a time destroy is called are deallocated via supplied deallocator if it's not null
