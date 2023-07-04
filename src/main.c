56b4bb65642e357615837baab2b1a626cd51372a
vadniks <whatever@fake.some>
2023-07-04 16:55:56 +0300

    Structure, logic, UI & UX

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 
 #include <stdlib.h>
+#include <sdl/SDL_stdinc.h>
+#include <assert.h>
 #include "lifecycle.h"
 
 int main(int argc, const char** argv) {
     if (!lifecycleInit(argc, argv)) return EXIT_FAILURE;
     lifecycleLoop();
     lifecycleClean();
+    assert(!SDL_GetNumAllocations());
     return EXIT_SUCCESS;
 }
