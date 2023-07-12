d4bbbb6ba8c2863bd5ca775131536c3298bb0110
vadniks <whatever@fake.some>
2023-07-12 16:35:58 +0300

    Structure, logic, UI & UX

--- src/defs.h
+++ src/defs.h
@@ -8,7 +8,7 @@
 #define min(x, y) (x < y ? x : y)
 #define max(x, y) (x > y ? x : y)
 #define boolToStr(x) (x ? "true" : "false")
-#define printArray(x, y) puts(#x " "); for (unsigned i = 0; i < (y); printf("%d ", (x)[i++])); puts("");
+#define printArray(x, y) puts(#x); for (unsigned i = 0; i < (y); printf("%d ", (x)[i++])); puts("");
 
 #define staticAssert(x) _Static_assert(x, "")
 
