30452437e0455772f38632159dcfcf502d8d9947
vadniks <whatever@fake.some>
2023-07-15 19:32:40 +0300

    Structure & logic

--- src/database/database.c
+++ src/database/database.c
@@ -1,29 +1,44 @@
 
 //#include "sqlite3proxy.h" // precompiled version of this header is included automatically by cmake
 #include <sdl/SDL_stdinc.h>
-#include <sdl/SDL_rwops.h>
 #include <assert.h>
 #include <unistd.h>
 #include "crypto.h"
 #include "database.h"
 
-STATIC_CONST_STRING FILE_NAME = "database.sqlite3";
+STATIC_CONST_STRING FILE_NAME = "./database.sqlite3";
+
+STATIC_CONST_STRING SERVICE_TABLE = "service"; // 7
+STATIC_CONST_STRING STREAMS_STATES_COLUMN = "streamsStates"; // 13
 
 THIS(
     sqlite3* db;
     Crypto* crypto;
 )
 
-static void initFromExisted(byte* passwordBuffer, unsigned size) {
-    byte streamsStates[CRYPTO_STREAMS_STATES_SIZE];
-//    SDL_LoadFile() // TODO
-
+static Crypto* init(byte* passwordBuffer, unsigned size, const byte* nullable streamsStates) {
+    Crypto* crypto = cryptoInit();
     byte* key = cryptoMakeKey(passwordBuffer, size);
+    cryptoSetUpAutonomous(crypto, key, streamsStates);
+    cryptoFillWithRandomBytes(key, CRYPTO_KEY_SIZE);
     SDL_free(key);
+    return crypto;
 }
 
-static void init(byte* passwordBuffer, unsigned size) {
+static Crypto* initFromExisted(byte* passwordBuffer, unsigned size) {
+    const unsigned sqlSize = 34;
+    char sql[sqlSize];
+    assert(SDL_snprintf(sql, sqlSize, "select %s from %s", STREAMS_STATES_COLUMN, SERVICE_TABLE) == sqlSize);
 
+    sqlite3_stmt* statement;
+    assert(!sqlite3_prepare(this->db, sql, (int) sqlSize, &statement, NULL));
+    assert(sqlite3_step(statement) == SQLITE_ROW);
+
+    const byte* streamsStates = sqlite3_column_blob(statement, 0);
+    assert(streamsStates && sqlite3_column_bytes(statement, 0) == (int) CRYPTO_STREAMS_STATES_SIZE);
+    assert(!sqlite3_finalize(statement));
+
+    return init(passwordBuffer, size, NULL);
 }
 
 bool databaseInit(byte* passwordBuffer, unsigned size) {
@@ -33,7 +48,7 @@ bool databaseInit(byte* passwordBuffer, unsigned size) {
     if (sqlite3_open(FILE_NAME, &(this->db)) != 0) return false;
 
     if (existedEarlier) initFromExisted(passwordBuffer, size);
-    else init(passwordBuffer, size);
+    else init(passwordBuffer, size, NULL);
 
     cryptoFillWithRandomBytes(passwordBuffer, size);
     SDL_free(passwordBuffer);
