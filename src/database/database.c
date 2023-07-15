1fad100f82a31baa9af394dc0e923b6d9df9bd3d
vadniks <whatever@fake.some>
2023-07-15 20:36:04 +0300

    Structure & logic

--- src/database/database.c
+++ src/database/database.c
@@ -1,6 +1,7 @@
 
 //#include "sqlite3proxy.h" // precompiled version of this header is included automatically by cmake
 #include <sdl/SDL_stdinc.h>
+#include <sdl/SDL_log.h>
 #include <assert.h>
 #include <unistd.h>
 #include "crypto.h"
@@ -87,10 +88,8 @@ bool databaseInit(byte* passwordBuffer, unsigned size) {
     bool existedEarlier = !access(FILE_NAME, F_OK | R_OK | W_OK);
     if (sqlite3_open(FILE_NAME, &(this->db)) != 0) return false;
 
-    Crypto* crypto = NULL;
-    if (existedEarlier) crypto = initFromExisted(passwordBuffer, size);
-    else crypto = init(passwordBuffer, size, NULL);
-    cryptoDestroy(crypto); // TODO
+    if (existedEarlier) this->crypto = initFromExisted(passwordBuffer, size);
+    else this->crypto = init(passwordBuffer, size, NULL);
 
     cryptoFillWithRandomBytes(passwordBuffer, size);
     SDL_free(passwordBuffer);
@@ -100,5 +99,6 @@ bool databaseInit(byte* passwordBuffer, unsigned size) {
 
 void databaseClean(void) {
     assert(this && !sqlite3_close(this->db));
+    cryptoDestroy(this->crypto);
     SDL_free(this);
 }
