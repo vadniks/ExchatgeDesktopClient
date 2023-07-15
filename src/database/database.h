fb212e8cb3e7c5eeef3ee9ef09f78571bd0d01f4
vadniks <whatever@fake.some>
2023-07-15 22:04:41 +0300

    Structure & logic

--- src/database/database.h
+++ src/database/database.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include "../defs.h"
 
-bool databaseInit(byte* passwordBuffer, unsigned size); // returns true on success, 'size'-sized buffer is filled with random bytes after initiating and is freed afterwords
+bool databaseInit(byte* passwordBuffer, unsigned size); // returns true on success (either true or false cleanup is needed to be performed anyway), 'size'-sized buffer is filled with random bytes after initiating and is freed afterwords
 bool databaseAddUser(unsigned id);
 bool databaseGetUser(unsigned id);
 void databaseClean(void);
