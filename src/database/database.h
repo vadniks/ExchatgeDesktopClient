8f5a8c119f4769c25f34f0b7fe7420ca29eff061
vadniks <whatever@fake.some>
2023-07-16 17:46:58 +0300

    Structure & logic

--- src/database/database.h
+++ src/database/database.h
@@ -3,14 +3,16 @@
 
 #include <stdbool.h>
 #include "../defs.h"
+#include "../collections/list.h"
 
 struct DatabaseMessage_t;
 typedef struct DatabaseMessage_t DatabaseMessage;
 
+DatabaseMessage* databaseMessageCreate(unsigned* nullable userId, unsigned long timestamp, const char* text, unsigned size); // userId may be null if the message came from current user, 0 < size <= NET_MESSAGE_BODY_SIZE
+void databaseMessageDestroy(DatabaseMessage* message);
+
 bool databaseInit(byte* passwordBuffer, unsigned size); // returns true on success (either true or false returned, cleanup is needed to be performed anyway), 'size'-sized buffer is filled with random bytes after initiating and is freed afterwords
 bool databaseAddUser(unsigned id); // returns true on success
-DatabaseMessage* databaseCreateMessage(unsigned* nullable userId, unsigned long timestamp, const char* text, unsigned size); // userId may be null if the message came from current user, 0 < size <= NET_MESSAGE_BODY_SIZE
-void databaseDestroyMessage(DatabaseMessage* message);
 bool databaseAddMessage(const DatabaseMessage* message); // returns true on success
-DatabaseMessage** nullable databaseGetMessages(unsigned userId, unsigned* size); // returns an array of messages (which is expected to be deallocated by the caller) on success; size is pointer to buffer in order to obtain array's size
+List* nullable databaseGetMessages(unsigned userId, unsigned* size); // returns an array of messages <DatabaseMessage*> (which is expected to be deallocated by the caller) on success; size is pointer to buffer in order to obtain array's size
 void databaseClean(void);
