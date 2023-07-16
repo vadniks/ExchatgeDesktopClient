955e5403753d1073e64e4fb88b1cb3081b74d872
vadniks <whatever@fake.some>
2023-07-16 17:21:21 +0300

    Structure & logic

--- src/database/database.h
+++ src/database/database.h
@@ -7,7 +7,7 @@
 struct DatabaseMessage_t;
 typedef struct DatabaseMessage_t DatabaseMessage;
 
-bool databaseInit(byte* passwordBuffer, unsigned size); // returns true on success (either true or false cleanup is needed to be performed anyway), 'size'-sized buffer is filled with random bytes after initiating and is freed afterwords
+bool databaseInit(byte* passwordBuffer, unsigned size); // returns true on success (either true or false returned, cleanup is needed to be performed anyway), 'size'-sized buffer is filled with random bytes after initiating and is freed afterwords
 bool databaseAddUser(unsigned id); // returns true on success
 DatabaseMessage* databaseCreateMessage(unsigned* nullable userId, unsigned long timestamp, const char* text, unsigned size); // userId may be null if the message came from current user, 0 < size <= NET_MESSAGE_BODY_SIZE
 void databaseDestroyMessage(DatabaseMessage* message);
