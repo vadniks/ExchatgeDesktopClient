d7ae2aded2103d2d95af8233826c72e34b10739e
vadniks <whatever@fake.some>
2023-07-16 20:10:08 +0300

    Structure & logic

--- src/database/database.h
+++ src/database/database.h
@@ -12,7 +12,7 @@ DatabaseMessage* databaseMessageCreate(unsigned* nullable userId, unsigned long 
 void databaseMessageDestroy(DatabaseMessage* message);
 
 bool databaseInit(byte* passwordBuffer, unsigned size); // returns true on success (either true or false returned, cleanup is needed to be performed anyway), 'size'-sized buffer is filled with random bytes after initiating and is freed afterwords
-bool databaseAddUser(unsigned id); // returns true on success
+bool databaseAddUser(unsigned id, const byte* streamsStates); // returns true on success
 bool databaseAddMessage(const DatabaseMessage* message); // returns true on success
 List* nullable databaseGetMessages(unsigned userId, unsigned* size); // returns an array of messages <DatabaseMessage*> (which is expected to be deallocated by the caller) on success; size is pointer to buffer in order to obtain array's size
 void databaseClean(void);
