86a2af26ee2fced0c189c914d0273e3ed7bf8a33
vadniks <whatever@fake.some>
2023-07-19 22:53:59 +0300

    Connecting the database

--- src/logic.c
+++ src/logic.c
@@ -190,7 +190,7 @@ static void onConversationSetUpInviteReceived(unsigned fromId) {
     lifecycleAsync((LifecycleAsyncActionFunction) &replyToConversationSetUpInvite, xFromId, 0);
 }
 
-static void processCredentials(const void* const* data) {
+static void processCredentials(void** data) {
     const char* username = data[0];
     const char* password = data[1];
     bool logIn = *((bool*) data[2]);
