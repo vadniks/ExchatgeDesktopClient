87fb77ce606c05a60fe0a5f4508b8038001cdf81
vadniks <whatever@fake.some>
2023-09-04 21:32:29 +0300

    Adjustments

--- src/render/xnuklear.c
+++ src/render/xnuklear.c
@@ -3,23 +3,17 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCUnusedMacroInspection"
 
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_INCLUDE_STANDARD_BOOL
-#define INCLUDE_STYLE
 #define NK_IMPLEMENTATION
 #define NK_SDL_RENDERER_IMPLEMENTATION
 
 #pragma clang diagnostic pop
 
+////////////////////////////////////////////////////////
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Weverything"
 
+#include "xnuklearDefs.h"
 #include <nuklear.h>
 #include <nuklear_sdl_renderer.h>
 
--- src/render/xnuklear.h
+++ src/render/xnuklear.h
@@ -2,21 +2,7 @@
 
 #pragma once
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "OCUnusedMacroInspection"
-
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_INCLUDE_STANDARD_BOOL
-#define INCLUDE_STYLE
-
-#pragma clang diagnostic pop
-
+#include "xnuklearDefs.h"
 #include <nuklear.h>
 #include <nuklear_sdl_renderer.h>
 
