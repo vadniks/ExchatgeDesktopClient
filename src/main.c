6004b21d2c5b703ca6565bb7f489a6e960194ef6
vadniks <whatever@fake.some>
2023-10-22 17:45:56 +0300

    Cleanup

--- src/main.c
+++ src/main.c
@@ -20,24 +20,38 @@
 #include <SDL.h>
 #include <assert.h>
 #include <stdio.h>
-//#include "lifecycle.h" // TODO: test only
-
-#include "defs.h"
+#include "lifecycle.h"
 
 int main(void) {
-    const unsigned A = 16;
-    byte key[A];
-    const long hostId;
-    for (unsigned i = 0; i < sizeof(long); ((byte*) &hostId)[i] = i, printf("%d", i++));
-    printf("\n");
+    if (!lifecycleInit()) return EXIT_FAILURE;
 
-    printBinaryArray(((byte*) &hostId), sizeof(long))
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "\r\033[1;32m"
+        "_______ _     _ _______ _     _ _______ _______  ______ _______\n"
+        "|______  \\___/  |       |_____| |_____|    |    |  ____ |______\n"
+        "|______ _/   \\_ |_____  |     | |     |    |    |_____| |______\n"
+        "                   free software (GNU GPL v3)                   \033[0m"
+    );
 
-//    for (unsigned i = 0; i < A; key[i] = (i < sizeof(long) ? ((byte*) &hostId)[i] : 0), printf("%d", key[i++]));
-    for (unsigned i = 0; i < A; key[i] = ((byte*) &hostId)[i % sizeof(long)], printf("%d", key[i++]));
-    printf("\n");
+    SDL_version version;
+    SDL_GetVersion(&version);
+    assert(version.major == 2);
 
-    printBinaryArray(key, A)
+    bool untested = false;
+    if (version.major * 1000 + version.minor * 10 + version.patch != 2265) {
+        untested = true;
+        SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "Tested only on 2.26.5, may work unstable on other versions"); // works on 2.28.1 but there are bugs in it
+    }
+
+    lifecycleLoop();
+    lifecycleClean();
+
+    const int allocations = SDL_GetNumAllocations();
+
+    if (allocations) // TODO: debug only
+        fprintf(stderr, "allocations %d\n", allocations); // Cannout use SDL_Log() here 'cause it will allocate smth internally and the cleanup has been performed before
+
+    assert(!allocations || untested && allocations == 1);
+    // unknown bug occurs on 2.28.1: SDL_GetNumAllocations() returns 1 everytime only SDL_Init() was called with SDL_INIT_VIDEO constant (after SDL_Quit() was called). on 2.26.5 it returns 0 as expected
 
     return EXIT_SUCCESS;
 }
