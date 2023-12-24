efb9ac7f042659dc0d9ef1bd95403eb55a09e0dd
vadniks <whatever@fake.some>
2023-12-24 17:55:48 +0300

    Bugs fixed

--- src/net.c
+++ src/net.c
@@ -72,6 +72,7 @@ typedef enum : int {
     FLAG_EXCHANGE_HEADERS_DONE = 0x000000d0, // A receives the B's encoder header, creates decoder and encoder, then A sends his encoder header to B
     // B receives A's header and creates decoder stream. After that, both A and B have keys and working encoders/decoders to begin an encrypted conversation
 
+    // TODO: create new Crypto (new cipher instance) for each file to be exchanged to avoid overuse of the main (conversation's) one as one cipher can handle only 0x7fffffff encryption operations
     FLAG_FILE_ASK = 0x000000e0, // firstly current user (A) sends file exchanging invite (flag_file_ask) with size == sizeof(file) to another user (B); if B accepts the invitation, he sends back to A flag_file_ask with size == sizeof(file), if he declines size == 0;
     FLAG_FILE = 0x000000f0, // secondly if B accepted the invite, A can proceed: A reads file by net_message_body_size-sized chunks, encapsulates those chunks in messages and sends them to B; B then accepts them, reads & writes those chunks to a newly created file
 
